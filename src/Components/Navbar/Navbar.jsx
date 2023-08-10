3ac0873d8fd0d6e73dd117823806ac0fa74f19e0
aayush <ayushwasnik999@gmail.com>
2023-08-10 16:27:27 +0530

    added navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import TuneIcon from "@mui/icons-material/Tune";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AppsRoundedIcon from "@mui/icons-material/AppsRounded";
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.left}>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="	https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png"
+          alt="Gmail"
+        />
+      </div>
+
+      <div className={styles.middle}>
+        <IconButton>
+          <SearchIcon className={styles.search_icon} />
+        </IconButton>
+
+        <input type="text" placeholder="Search mail" />
+        <IconButton>
+          <TuneIcon />
+        </IconButton>
+      </div>
+
+      <div className={styles.right}>
+        <IconButton>
+          <HelpOutlineIcon />
+        </IconButton>
+
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+
+        <IconButton>
+          <AppsRoundedIcon />
+        </IconButton>
+
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
